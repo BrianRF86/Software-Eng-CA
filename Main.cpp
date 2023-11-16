34dc97d4a007fdba0e9608440792a05e33fa1b6d
BrianRF86 <brianruiz.flynn@live.com>
2023-11-16 00:23:32 +0000

    Player class created

--- Main.cpp
+++ Main.cpp
@@ -4,23 +4,96 @@
 
 #include "raymath.h"
 
-//Fixing texture https://github.com/raysan5/raylib/blob/master/examples/textures/textures_background_scrolling.c
+
 
 //objects
 
-struct crosshair{
+// Preventing the player from leaving the screen. 
 
-Texture2D texture;
-Rectangle circ;
-Vector2 position;
+class player{
+
+protected:
+
+//Setting player details
+player.width = screenWidth/4;
+player.height = screenHeight/4;
+player.x = screenWidth/2;
+player.y = screenHeight/2;
+
+// Preventing the player from leaving the screen. 
+
+void Limitmovement(){
+if(y <= 0)
+    {
+        y = 0;
+    }
+
+if (y + height >= GetScreenHeight()) 
+    {
+        y = GetScreenHeight - height;
+    }
+
+if(x <=0)
+{
+    x = 0;
+}
+
+if(x + width >= GetScreenWidth())
+{
+    x = GetScreenWidth - width;
+}
+
+}
+
+
+    float x,y;
+    float width, height;
+    
+
+void Loadtexture(Player);
+
+// Player movement & Map navigation on mouse click right 
+
+
+if(IsMouseButtonDown(MOUSE_BUTTON_RIGHT))
+        { Vector2 delta = GetMouseDelta();
+        delta = Vector2Scale(delta, -1.0f/player.zoom);
+        player.target = Vector2Add(player.target, delta);
+        }
+        // Zoom function on mouse wheel
+        float wheel = GetMouseWheelMove();
+        if (wheel != 0)
+        {
+
+// For zoom function from Raylib example  Copyright (c) 2022-2023 Jeffery Myers (@JeffM2501) https://github.com/raysan5/raylib/blob/master/examples/core/core_2d_camera_mouse_zoom.c
+
+Camera2D player = { 0 }; 
+Player.zoom = 1.0f;
+
+     //World point is at mouse position.
+
+            Vector2 mouseWorldPos = GetScreenToWorld2D(GetMousePosition(), Player);
+           
+
+            //Set the offset to where the mouse is
+            Player.offset = GetMousePosition();
+
+         // Set the target to match, so that the camera maps the world space point 
+        // under the cursor to the screen space point under the cursor at any zoom
+            Player.target = mouseWorldPos;
+    
 
 };
+
+player player;
 
 int main() {
     // Determine the Game Window Width and Height
     // changed screen size to match widescreen laptops
-    const int screenWidth = 1280;
-    const int screenHeight = 720;
+    const int screenWidth = 832;
+    const int screenHeight = 430;
+
+//Fixing texture https://github.com/raysan5/raylib/blob/master/examples/textures/textures_background_scrolling.c
 
     // Initialize the Window
     InitWindow(screenWidth, screenHeight, "Saving President Chump");
@@ -28,60 +101,29 @@ int main() {
 //Background Texture
 Texture2D background = LoadTexture("resources/Street.png");
 
-//Crosshair texture
-Texture2D crosshair = LoadTexture("resources/crosshair.png");
 
-
-float scrollingBackground = 0.0f;
-
-// Camera class - For zoom function from Raylib example  Copyright (c) 2022-2023 Jeffery Myers (@JeffM2501) https://github.com/raysan5/raylib/blob/master/examples/core/core_2d_camera_mouse_zoom.c
-
-Camera2D camera = { 0 }; 
-camera.zoom = 1.0f;
 
     // Setting the Frames Per Second
     SetTargetFPS(60);
+
+
+
+
 
 
     // The Game Loop
     while (!WindowShouldClose() /*WindowShouldClose returns true if esc is clicked and closes the window*/) {
 
         //Updates
-        //Fixing texture https://github.com/raysan5/raylib/blob/master/examples/textures/textures_background_scrolling.c
-        scrollingBackground -= 0.1f;
 
-        crosshair cursor = (crosshair){
-            .texture = crosshair,
-            .position = GetMousePosition (),
-            .circ = {0.0f, 0.0f, 64.0f, 64.f},
-        };
-
-        //Map navigation on mouse click right 
-        if(IsMouseButtonDown(MOUSE_BUTTON_RIGHT))
-        { Vector2 delta = GetMouseDelta();
-        delta = Vector2Scale(delta, -1.0f/camera.zoom);
-        camera.target = Vector2Add(camera.target, delta);
-        }
-        // Zoom function on mouse wheel
-        float wheel = GetMouseWheelMove();
-        if (wheel != 0)
-        {
-            
-            //World point is at mouse position.
-            Vector2 mouseWorldPos = GetScreenToWorld2D(GetMousePosition(), camera);
-
-            //Set the offset to where the mouse is
-            camera.offset = GetMousePosition();
-
-         // Set the target to match, so that the camera maps the world space point 
-        // under the cursor to the screen space point under the cursor at any zoom
-            camera.target = mouseWorldPos;
-
+     
+         
+       
         // Zoom increase
         const float zoomIncrease = 0.125f;
 
-        camera.zoom += (wheel*zoomIncrease);
-        if(camera.zoom < zoomIncrease) camera.zoom = zoomIncrease;
+        Player.zoom += (wheel*zoomIncrease);
+        if(Player.zoom < zoomIncrease) Player.zoom = zoomIncrease;
 
         }
         
@@ -92,14 +134,14 @@ camera.zoom = 1.0f;
         // Clear canvas to a specific color to avoid flicker
         ClearBackground(RAYWHITE);
         // initiaing 2D mode
-         BeginMode2D(camera);
+         BeginMode2D(Player);
 
     
-        //Fixing texture https://github.com/raysan5/raylib/blob/master/examples/textures/textures_background_scrolling.c
-        DrawTextureEx(background, { scrollingBackground, 20 }, 0.0f, 2.0f, WHITE);
-        DrawTextureEx(background, { background.width*2 + scrollingBackground, 20 }, 0.0f, 2.0f, WHITE);
+  
+        Loadtexture(background,0.0f, 2.0f, WHITE);
+        
 
-        DrawTexture(crosshair,0.0f,2.0f,WHITE);
+       
 
         // Ending 2D mode
         EndMode2D();
@@ -110,7 +152,7 @@ camera.zoom = 1.0f;
         //De-Initialization
 //Fixing texture https://github.com/raysan5/raylib/blob/master/examples/textures/textures_background_scrolling.c
 UnloadTexture(background); 
-UnloadTexture(crosshair);
+UnloadTexture(Player);
 
     CloseWindow();
     return 0;
