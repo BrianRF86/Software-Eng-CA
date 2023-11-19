6038478204df527cae7da9d364d960135515ef18
BrianRF86 <brianruiz.flynn@live.com>
2023-11-19 01:54:06 +0000

    Player texture

--- Main.cpp
+++ Main.cpp
@@ -1,100 +1,53 @@
 #include "raylib.h"
 
-// Added raymath to resolve vector errors from https://github.com/raysan5/raylib/blob/master/src/raymath.h
-
-#include "raymath.h"
-#include "player.h"
-
-//objects
-
-//defining 
-
-// Preventing the player from leaving the screen. 
-
-
 int main() {
-    // Determine the Game Window Width and Height
-    // changed screen size to match widescreen laptops
-    const int screenWidth = 832;
-    const int screenHeight = 430;
-
-
-//Fixing texture https://github.com/raysan5/raylib/blob/master/examples/textures/textures_background_scrolling.c
+    // Determin the Game Window Width and Height
+    const int screenWidth = 800;
+    const int screenHeight = 450;
 
     // Initialize the Window
-    InitWindow(screenWidth, screenHeight, "Saving President Chump");
+    InitWindow(screenWidth, screenHeight, "Scarfy's dodgeball survival");
 
-         // Player movement & Map navigation on mouse click right --- moving back to INT section https://github.com/raysan5/raylib/blob/master/examples/core/core_2d_camera_mouse_zoom.c
+//https://www.youtube.com/watch?v=j0C4ox1gFxk&list=PLORJX3OiHbbMs9AFM5bzpNUychJm1raub
 
-//Background Texture
-Texture2D background = LoadTexture("resources/Street.png");
+Texture2D playertex = LoadTexture("resources/scarfy.png");
+unsigned numFrames = 6;
+	int frameWidth = playertex.width / numFrames;
+	Rectangle frameRec = { 0.0f, 0.0f, (float)frameWidth, (float)playertex.height };
+	Vector2 playerPosition = {screenWidth / 2.0f, screenHeight / 2.0f};
+    Vector2 playerVelocity = {0.0f,0.0f};
+	
+	unsigned frameDelay = 5;
+	unsigned frameDelayCounter = 0;
+	unsigned frameIndex = 0;
 
     // Setting the Frames Per Second
     SetTargetFPS(60);
 
-
     // The Game Loop
     while (!WindowShouldClose() /*WindowShouldClose returns true if esc is clicked and closes the window*/) {
 
-        //Updates
-         // Translate based on mouse right click
-        if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT))
-        {
-            Vector2 delta = GetMouseDelta();
-            delta = Vector2Scale(delta, -1.0f/camera.zoom);
 
-            camera.target = Vector2Add(camera.target, delta);
+            //updates
+
+          	++frameDelayCounter;
+		if(frameDelayCounter > frameDelay) {
+			frameDelayCounter = 0;
         }
+    
 
-        // Zoom based on mouse wheel
-        float wheel = GetMouseWheelMove();
-        if (wheel != 0)
-        {
-            // Get the world point that is under the mouse
-            Vector2 mouseWorldPos = GetScreenToWorld2D(GetMousePosition(), camera);
-            
-            // Set the offset to where the mouse is
-            camera.offset = GetMousePosition();
-
-            // Set the target to match, so that the camera maps the world space point 
-            // under the cursor to the screen space point under the cursor at any zoom
-            camera.target = mouseWorldPos;
-
-            // Zoom increment
-            const float zoomIncrement = 0.125f;
-
-            camera.zoom += (wheel*zoomIncrement);
-            if (camera.zoom < zoomIncrement) camera.zoom = zoomIncrement;
-        }
-        
         // Setup Canvas
         BeginDrawing();
-        
-        // Here goes all the Game Logic
         // Clear canvas to a specific color to avoid flicker
         ClearBackground(RAYWHITE);
-   
-    
-
-    
-  
-        DrawTexture(background,0.0f, 2.0f, WHITE);
-        DrawTexture(player,0.0f, 2.0f,WHITE);
 
        
-
-        // Ending 2D mode
-        EndMode2D();
+        DrawTextureRec(playertex, frameRec, playerPosition,WHITE);
+        // Here goes all the Game Logic
 
         // teardown Canvas
         EndDrawing();
     }
-        //De-Initialization
-//Fixing texture https://github.com/raysan5/raylib/blob/master/examples/textures/textures_background_scrolling.c
-UnloadTexture(background); 
-
-
     CloseWindow();
     return 0;
 }
-
