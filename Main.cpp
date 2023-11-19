5900e64ce732d8cf9187c44724645c73f9478f3b
BrianRF86 <brianruiz.flynn@live.com>
2023-11-19 19:02:31 +0000

    Ball update

--- Main.cpp
+++ Main.cpp
@@ -12,6 +12,41 @@ const int jumpDownFrame = 4;
 const int footstepFrame1 = 1;
 const int footstepFrame2 = 4;
 
+
+//Ball https://github.com/BrianRF86/Git-hub-project/commit/ae34663f72f7e8ede5a7a12281c4a4ca0339929c
+
+class Ball {
+public: 
+float x, y;
+int speed_x, speed_y;
+int radius;
+
+void Draw () {
+
+    DrawCircle(x, y, radius, YELLOW);
+
+    }
+
+
+//ball movement
+
+void Update() {
+x += speed_x;
+y += speed_y;
+
+
+// Border collision
+
+ if (y + radius >= GetScreenHeight() || y - radius <=0)
+    {
+            speed_y *= -1;
+    }
+ if (x + radius >= GetScreenWidth() || x - radius <=0)
+    {
+            speed_x *= -1;
+    }
+};
+};
 // Ground boolean https://www.youtube.com/watch?v=_JjPo8rE8a8&t=29s
 
 bool isplayerOnGround(Vector2 *playerPosition){
@@ -31,7 +66,7 @@ int main() {
     const int gravity = 1;
     //Defining Player speed
 
-    
+
     // Initialize the Window
     InitWindow(screenWidth, screenHeight, "Scarfy's dodgeball survival");
 
@@ -51,15 +86,25 @@ unsigned numFrames = 6;
 
 
 
+    Ball ball;
 
     // Setting the Frames Per Second
     SetTargetFPS(60);
 
+ ball.radius = 20;
+    ball.x = screenWidth/2;
+    ball.y = screenHeight/2;
+    ball.speed_x = 7;
+    ball.speed_y = 7;
+ 
     // The Game Loop
     while (!WindowShouldClose() /*WindowShouldClose returns true if esc is clicked and closes the window*/) {
 
 
             //updates
+
+
+     ball.Update();
 
         //fix animation speed https://github.com/raysan5/raylib/blob/master/examples/textures/textures_sprite_anim.c
 
@@ -124,14 +169,10 @@ unsigned numFrames = 6;
 
 // stretching background to fit screen https://www.youtube.com/watch?time_continue=11&v=AKTLg1SWfG0&embeds_referring_euri=https%3A%2F%2Fwww.google.com%2F&source_ve_path=Mjg2NjY&feature=emb_logo &
 // https://stackoverflow.com/questions/75932497/trouble-drawing-texture-using-drawtexturepro-in-raylib-c-to-centralise-it
-       DrawTexturePro(
-    background,
-    (Rectangle){ 0.0f, 0.0f, (float)background.width, (float)background.height },
-    (Rectangle){ 0.0f, 0.0f, (float)screenWidth, (float)screenHeight },
-    (Vector2){ 0.0f, 0.0f },
-    0.0f,
-    WHITE);
+       DrawTexturePro(background,(Rectangle){ 0.0f, 0.0f, (float)background.width, (float)background.height },
+       (Rectangle){ 0.0f, 0.0f, (float)screenWidth, (float)screenHeight },(Vector2){ 0.0f, 0.0f }, 0.0f,WHITE);
 
+    ball.Draw();
 
         DrawTextureRec(playertex, frameRec, playerPosition,WHITE);
         // Here goes all the Game Logic
